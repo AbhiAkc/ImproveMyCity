e4aa0b4982fd46cb1ea8a50dfb04f7f9d045e053
Abhishek <shinichi212002@gmail.com>
2025-10-25 22:05:04 +0530

    Create complaint_app and update INSTALLED_APPS with rest_framework and complaint_app

--- city_app/settings.py
+++ city_app/settings.py
@@ -38,9 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',  # For API
-    'complaints',      # New app
+    'complaint_app',   # New app
 ]
-
 
 
 MIDDLEWARE = [
